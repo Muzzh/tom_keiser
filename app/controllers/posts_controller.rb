2016228316c18743c6024f0ab252f1d73797d279
Muzzh <murdyn@gmail.com>
2018-01-02 23:04:55 +0000

    Posts creation and display

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,30 @@
 class PostsController < ApplicationController
 
   def index
-    
+    @posts = Post.all
   end
+
+  def new
+    @post = Post.new
+    unless current_user.admin == true
+      flash[:notice] = 'You do not have posting rights'
+      redirect_to root_path
+    end
+  end
+
+  def create
+    @post = Post.create(post_params)
+    redirect_to post_path(@post)
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
+
 end
