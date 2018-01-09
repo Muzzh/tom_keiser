cfa0a96aa6c29756a356d72f530bb96686eea9ed
Muzzh <murdyn@gmail.com>
2018-01-09 01:42:49 +0000

    another try

--- config/application.rb
+++ config/application.rb
@@ -6,12 +6,12 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-config.autoload_paths += %W(#{config.root}/app/models/ckeditor)
-config.assets.precompile += Ckeditor.assets
-config.assets.precompile += %w(ckeditor/*)
 
 module TomKeiser
   class Application < Rails::Application
+    config.autoload_paths += %W(#{config.root}/app/models/ckeditor)
+    config.assets.precompile += Ckeditor.assets
+    config.assets.precompile += %w(ckeditor/*)
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
