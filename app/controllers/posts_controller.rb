392415ad61baf536df7418965ee3cca2305c6341
Muzzh <murdyn@gmail.com>
2018-01-12 22:37:29 +0000

    fix update post and add 2 pdf articles provided by Tom

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -31,6 +31,16 @@ class PostsController < ApplicationController
     end
   end
 
+  def update
+    unless current_user.admin?
+      flash[:notice] = 'You do not have posting rights'
+      redirect_to root_path
+    end
+    @post = Post.find(params[:id])
+    @post.update_attributes(post_params)
+    redirect_to post_path(@post)
+  end
+
   def show
     @post = Post.find(params[:id])
   end
